5163f9af0dcbed8ec761b4c85891e41414b92a7e
T.C. Cox <thmscly@gmail.com>
2022-12-12 21:27:23 -0500

    fixed media points in About, shrunk images in Home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <div className="sticky top-0 bg-slate-100 h-20 min-w-screen">
-            <div className=' grid grid-flow-col items-center gap-4 text-center text-slate-700 font-italiana h-full'>
+        <div className="sticky top-0 bg-slate-100 h-24 min-w-screen">
+            <div className=' grid grid-flow-col items-center gap-5 text-center text-slate-700 font-italiana h-full'>
                 <div className=''>
                     <a href='/'><h1 className='text-2xl text-slate-700 text-center font-tangerine p-2 drop-shadow-xl sm:text-6xl'>Katie DeVault</h1></a>
                 </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,12 +3,12 @@ import GirlFashion from '../../images/GirlFashion.jpg'
 
 function About() {
     return (
-        <div className='m-28 mt-12 text-slate-800 font-italiana text-xl'>
+        <div className='sm:m-28 sm:mt-12 m-5 mt-2 text-slate-800 font-italiana text-xl'>
             <h1 className='text-4xl text-center mb-5'>Katie DeVault</h1>
             <div className='grid grid-cols-5 content-center'>
                 <img className='w-full h-auto float-left col-span-2' src={GirlFashion} alt="" />
                 <p className='col-span-3 self-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p className='col-span-5 m-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='col-span-5 sm:m-8 m-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
     )
