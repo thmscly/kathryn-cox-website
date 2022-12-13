d5218a27b2a66dbe9be81bd58677a848cf5ad73c
T.C. Cox <thmscly@gmail.com>
2022-12-12 21:39:11 -0500

    changed nav w back to screen from fit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <div className="sticky top-0 bg-slate-100 h-24 w-fit">
+        <div className="sticky top-0 bg-slate-100 h-24 w-screen">
             <div className=' grid grid-flow-col items-center gap-5 text-center text-slate-700 font-italiana h-full'>
                 <div className='text-xl'>
                     <a href='/about'>About</a>
