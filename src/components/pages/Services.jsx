00e5ee2b84833de376dc93f20dae7446905d98ad
T.C. Cox <thmscly@gmail.com>
2022-12-16 12:28:59 -0500

    Formatted h2 in services/added lorem ipsum: objs

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -5,7 +5,7 @@ function Services() {
   return (
     <div className="m-28 mt-12 text-slate-800 font-italiana text-xl">
       <h1 className="text-4xl text-center mb-5">What I Do</h1>
-      <div className="grid grid-cols-1 text-center">
+      <div className="grid grid-cols-1 text-center divide-y">
         {serviceList.map((service) => (
           <ServiceBlock title={service.title} text={service.text} />
         ))}
