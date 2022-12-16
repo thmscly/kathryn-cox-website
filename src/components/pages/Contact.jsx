3f0941456833956b4718fb5c41a492b79f8989bf
T.C. Cox <thmscly@gmail.com>
2022-12-16 15:26:20 -0500

    formatted docs

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from "react";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Contact() {
-    return (
-        <div className="grid grid-cols-3 gap-1 text-center h-screen text-slate-800 font-italiana text-xl mt-20 w-screen">
-            <div className='m-3'>
-                <h2 className='font-bold'>Email</h2>
-                <p>email@email.com</p>
-                <EmailIcon />
-            </div>
-            <div className='m-3'>
-                <h2 className='font-bold'>Phone</h2>
-                <p>(123)456-7890</p>
-                <PhoneIphoneIcon />
-            </div>
-            <div className='m-3'>
-                <h2 className='font-bold'>LinkedIn</h2>
-                <p>linkedin.com/totes-profesh-yo</p>
-                <LinkedInIcon />
-            </div>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-3 gap-1 text-center h-screen text-slate-800 font-italiana text-xl mt-20 w-screen">
+      <div className="m-3">
+        <h2 className="font-bold">Email</h2>
+        <p>email@email.com</p>
+        <EmailIcon />
+      </div>
+      <div className="m-3">
+        <h2 className="font-bold">Phone</h2>
+        <p>(123)456-7890</p>
+        <PhoneIphoneIcon />
+      </div>
+      <div className="m-3">
+        <h2 className="font-bold">LinkedIn</h2>
+        <p>linkedin.com/totes-profesh-yo</p>
+        <LinkedInIcon />
+      </div>
+    </div>
+  );
 }
--- src/data/ServiceList.js
+++ src/data/ServiceList.js
@@ -3,24 +3,24 @@ import GirlFashion from "images/GirlFashion.jpg";
 
 export const serviceList = [
     {
-        "id": 1,
-        "title": "Consultation",
-        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
-        "img": GirlFashion
+        id: 1,
+        title: "Consultation",
+        shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
+        img: GirlFashion,
     },
     {
-        "id": 2,
-        "title": "Research",
-        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
-        "img": GirlFashion
+        id: 2,
+        title: "Research",
+        shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
+        img: GirlFashion,
     },
     {
-        "id": 3,
-        "title": "Fashion",
-        "shortDesc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
-        "img": GirlFashion
-    }
-]
+        id: 3,
+        title: "Fashion",
+        shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
+        img: GirlFashion,
+    },
+];
