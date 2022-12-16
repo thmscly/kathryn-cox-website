ddde449f725463373f5a7ecb56542d4071596205
T.C. Cox <thmscly@gmail.com>
2022-12-16 12:32:20 -0500

    issue#3: added id's to service objects

--- src/data/ServiceList.js
+++ src/data/ServiceList.js
@@ -1,13 +1,18 @@
+//add id to each object as added
+
 export const serviceList = [
     {
+        "id": 1,
         "title": "Consultation",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
     },
     {
+        "id": 2,
         "title": "Research",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
     },
     {
+        "id": 3,
         "title": "Fashion",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod orci vel tincidunt porttitor. Sed ut ante elementum, tincidunt nisl at, aliquet odio. Vivamus et ligula a nisi porta tempor. Vivamus interdum ipsum vel sapien dignissim, a ornare magna ultrices. Aliquam vulputate hendrerit arcu, ut blandit dolor mattis eget. Praesent porttitor orci et felis aliquet, id egestas dui maximus. Etiam a tincidunt ex, in vestibulum sapien. Maecenas ultricies, arcu vitae volutpat semper, diam est laoreet magna, eu eleifend eros neque in purus. Aenean semper est eu sem lobortis lacinia. Praesent nec sem dignissim, rutrum mi vel, lobortis sem. Nullam vel aliquam nisl. Donec justo mauris, iaculis eu dolor et, suscipit suscipit purus. Quisque gravida, nulla ac tempor ullamcorper, mi magna suscipit dolor, id finibus eros lectus aliquet arcu. Integer pellentesque velit quis dolor tristique sodales.",
     }
