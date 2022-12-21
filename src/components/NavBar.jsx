5ebbec9b757126a204af517eb53587ab2cce0569
T.C. Cox <thmscly@gmail.com>
2022-12-21 13:51:05 -0600

    Added vert lines in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="sticky top-0 bg-slate-100 h-16 sm:h-24 w-screen">
-      <div className=" grid grid-flow-col-dense items-center sm:gap-5 gap-1 text-center text-slate-700 font-italiana h-full">
+      <div className=" grid grid-flow-col-dense items-center sm:gap-5 gap-1 text-center text-slate-700 font-italiana h-full divide-x">
         <div className="sm:text-xl">
           <a href="/about">About</a>
         </div>
         <div className="sm:text-xl">
           <a href="/services">Services</a>
         </div>
-        <div className="sm:text-xl font-tangerine sm:text-4xl text-2xl">
+        <div className="sm:text-xl font-tangerine sm:text-5xl text-2xl">
           <a href="/">Katie DeVault</a>
         </div>
         <div className="sm:text-xl">
